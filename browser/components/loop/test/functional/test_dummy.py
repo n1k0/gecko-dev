29027f698d4c1d260c859a3f925dbed3dbfdb1d5
Dan Mosedale <dmose@meer.net>
2014-04-03 16:42:53 -0700

    Simplified test case on Marionette chrome testing

--- browser/components/loop/test/functional/test_dummy.py
+++ browser/components/loop/test/functional/test_dummy.py
@@ -1,19 +1,25 @@
 from marionette_test import MarionetteTestCase
 from by import By
+import time
 
 class TestSomething(MarionetteTestCase):
+
     def test_foo(self):
         
-        # this is browser chrome, kids, not the content window just yet
+        # this is browser chrome, not the content window just yet
         self.marionette.set_context("chrome")
 
-        self.marionette.find_element(By.ID, "social-status-button-browser")
+        # drop down the status panel
+        self.marionette.find_element(By.ID,
+                                     "social-status-button-browser").click()
 
-        # click the element
+        # ensure this isn't just that we're looking too early.  With this
+        # sleep, the panel is visible to the eye for multiple seconds before
+        # the frame switch is attempted and the exception is thrown.
+        time.sleep(5)
 
         # switch to the frame
+        panel_frame = self.marionette.find_element(By.CLASS_NAME,
+                                                   "social-panel-frame")
+        self.marionette.switch_to_frame(panel_frame)
 
-        # set context to content
-        self.marionette.set_context("content")
-
-        # find content element
