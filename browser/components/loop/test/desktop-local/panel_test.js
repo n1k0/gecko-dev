6f55d5ad563ec68e22115ae6e7747176b9318666
Dan Mosedale <dmose@meer.net>
2014-04-09 15:58:02 -0700

    Refactor to support shared and desktop-local frontend tests

--- browser/components/loop/test/desktop-local/panel_test.js
+++ browser/components/loop/test/desktop-local/panel_test.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* global loop, sinon */
+/*global loop, sinon */
 
 var expect = chai.expect;
 
@@ -79,7 +79,7 @@ describe("loop.panel", function() {
     describe("#onCallUrlReceived", function() {
       it("should update the text field with the call url", function() {
         var view = new loop.panel.PanelView();
-        view.render();
+        view.render()
 
         view.onCallUrlReceived("http://call.me/");
 
